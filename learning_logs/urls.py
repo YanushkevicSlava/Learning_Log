340685ce6c234128c0c4b1ed4cc231fd8b8a4208
Slava Yanuskevich <cool.9nyw@gmail.com>
2023-05-01 12:58:56 +0300

    add urls to learning_logs

---
+++ learning_logs/urls.py
@@ -0,0 +1,13 @@
+"""Определяет схемы URL для learning_logs."""
+
+from django.urls import path
+from . import views
+
+
+app_name = 'learning_logs'
+urlpatterns = [
+    # домашняя страница.
+    path('', views.index, name='index'),
+]
+
+
