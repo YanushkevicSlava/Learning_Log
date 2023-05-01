7b30a44d500fd2fff059c491eea29ba2d2459121
Slava Yanuskevich <cool.9nyw@gmail.com>
2023-05-01 17:02:08 +0300

    upgrade views

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    """Домашняя страница прложения learning_log"""
+    return render(request, 'learning_logs/index.html')
