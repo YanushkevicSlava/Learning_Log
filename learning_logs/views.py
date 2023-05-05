01f3e0a1d9588fc2ef0126b9b18d1ca6b734afc4
Slava Yanuskevich <cool.9nyw@gmail.com>
2023-05-05 05:12:14 +0300

    add view topic

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -8,8 +8,16 @@ def index(request):
     """Домашняя страница прложения learning_log"""
     return render(request, 'learning_logs/index.html')
 
+
 def topics(reqest):
     """Выводит список тем."""
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
     return render(reqest, 'learning_logs/topics.html', context)
+
+
+def topic(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
