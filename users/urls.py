0134b5af629c9781d72d2abec7504a1156de22f6
Slava Yanuskevich <cool.9nyw@gmail.com>
2023-05-13 19:32:20 +0300

    add new urls to users

---
+++ users/urls.py
@@ -0,0 +1,11 @@
+"""Определяет схемы URL для пользователей"""
+
+from django.urls import path, include
+
+
+app_name = 'users'
+
+urlpattrens = [
+    # Включить URL персонализации по умолчанию.
+    path('', include('django.contrib.auth.urls')),
+]
